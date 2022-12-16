9fec429f42f35f1e120fd383de4590ef35ff825f
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-12-16 13:29:40 +0530

    movies design updated

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -6,33 +6,41 @@ const Movies = () => {
     <div className='mt-6'>
       <p className='font-bold text-2xl'>Most Recent Movies</p>
 
-      <div className='grid grid-cols-4 gap-4'>
+      {/* <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 sm:gap-4'> */}
+      <div className='flex flex-wrap items-center justify-center'>
 
-        <div className='relative shadow-lg my-4 rounded-md h-[348px] w-[282px] col-span-1'>
+        <div className='relative shadow-lg rounded-md h-[348px] w-[282px] m-6 col-span-1'>
           <div className='absolute border border-black top-2 left-3 rounded-full bg-white h-[34px] w-[34px] flex justify-center items-center'>7.9</div>
           <img src={pathan} alt="movie" className='rounded-md h-[348px] w-[282px] object-cover' />
           <p className='absolute bottom-0 z-10 bg-white w-full text-center py-2 rounded-b-md'>Pathan</p>
         </div>
-        <div className='relative shadow-lg my-4 rounded-md h-[348px] w-[282px] col-span-1'>
+        <div className='relative shadow-lg rounded-md h-[348px] w-[282px] m-6 col-span-1'>
           <div className='absolute border border-black top-2 left-3 rounded-full bg-white h-[34px] w-[34px] flex justify-center items-center'>7.9</div>
           <img src={pathan} alt="movie" className='rounded-md h-[348px] w-[282px] object-cover' />
           <p className='absolute bottom-0 z-10 bg-white w-full text-center py-2 rounded-b-md'>Pathan</p>
         </div>
-        <div className='relative shadow-lg my-4 rounded-md h-[348px] w-[282px] col-span-1'>
+        <div className='relative shadow-lg rounded-md h-[348px] w-[282px] m-6 col-span-1'>
           <div className='absolute border border-black top-2 left-3 rounded-full bg-white h-[34px] w-[34px] flex justify-center items-center'>7.9</div>
           <img src={pathan} alt="movie" className='rounded-md h-[348px] w-[282px] object-cover' />
           <p className='absolute bottom-0 z-10 bg-white w-full text-center py-2 rounded-b-md'>Pathan</p>
         </div>
-        <div className='relative shadow-lg my-4 rounded-md h-[348px] w-[282px] col-span-1'>
+        <div className='relative shadow-lg rounded-md h-[348px] w-[282px] m-6 col-span-1'>
           <div className='absolute border border-black top-2 left-3 rounded-full bg-white h-[34px] w-[34px] flex justify-center items-center'>7.9</div>
           <img src={pathan} alt="movie" className='rounded-md h-[348px] w-[282px] object-cover' />
           <p className='absolute bottom-0 z-10 bg-white w-full text-center py-2 rounded-b-md'>Pathan</p>
         </div>
-        <div className='relative shadow-lg my-4 rounded-md h-[348px] w-[282px] col-span-1'>
+        <div className='relative shadow-lg rounded-md h-[348px] w-[282px] m-6 col-span-1'>
           <div className='absolute border border-black top-2 left-3 rounded-full bg-white h-[34px] w-[34px] flex justify-center items-center'>7.9</div>
           <img src={pathan} alt="movie" className='rounded-md h-[348px] w-[282px] object-cover' />
           <p className='absolute bottom-0 z-10 bg-white w-full text-center py-2 rounded-b-md'>Pathan</p>
         </div>
+        <div className='relative shadow-lg rounded-md h-[348px] w-[282px] m-6 col-span-1'>
+          <div className='absolute border border-black top-2 left-3 rounded-full bg-white h-[34px] w-[34px] flex justify-center items-center'>7.9</div>
+          <img src={pathan} alt="movie" className='rounded-md h-[348px] w-[282px] object-cover' />
+          <p className='absolute bottom-0 z-10 bg-white w-full text-center py-2 rounded-b-md'>Pathan</p>
+        </div>
+        
+
 
       </div>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,10 +5,10 @@ import search from "../Assests/search.svg"
 const Navbar = () => {
   return (
     <div className='flex justify-between border-[#C0C4CC] items-center border-b-[1px] pb-2'>
-      <img src={logo} alt="logo" />
-      <div className='flex border border-[#C0C4CC] py-1 px-3 items-center justify-center h-[35px]'>
+      <img src={logo} alt="logo" className='h-12 md:h-[54px]' />
+      <div className='flex border border-[#C0C4CC] py-1 px-3 items-center justify-center h-[35px] '>
         <img src={search} alt="searchico" className='h-[20px] mr-2' />
-        <input type="text" className='outline-none' placeholder='Search for a movie' />
+        <input type="text" className='outline-none w-[150px]' placeholder='Search for a movie' />
       </div>
     </div>
   )
