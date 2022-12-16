4b23622510537e1cb4af7dc27255c3231172ab36
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-12-16 12:38:27 +0530

    Components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Movies from "./Components/Movies";
+import Navbar from "./Components/Navbar";
+
 function App() {
 
   return (
     <div className="">
-      App
+      <Navbar />
+      <Movies />
     </div>
   );
 }
